65e26e98828b41a33371e38ed333d77294de1135
Olivia Li <oliviali.ouyang@gmail.com>
2023-11-03 02:21:11 -0400

    ssr false

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,25 +1,34 @@
 "use client"
-
 import { useEffect, useState, useRef } from "react"
-import { MapContainer, TileLayer, Marker, GeoJSON, Popup } from "react-leaflet"
-import L, { DivIcon, Icon, IconOptions, LatLngExpression } from "leaflet"
+import dynamic from "next/dynamic"
 import "leaflet/dist/leaflet.css"
+
+// Dynamically import react-leaflet components
+const MapContainer = dynamic(() => import("react-leaflet").then((mod) => mod.MapContainer), { ssr: false })
+const TileLayer = dynamic(() => import("react-leaflet").then((mod) => mod.TileLayer), { ssr: false })
+const Marker = dynamic(() => import("react-leaflet").then((mod) => mod.Marker), { ssr: false })
+const GeoJSON = dynamic(() => import("react-leaflet").then((mod) => mod.GeoJSON), { ssr: false })
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), { ssr: false })
+
+import { LatLngExpression, Icon, IconOptions, DivIcon } from "leaflet"
 
 const LocationMap = () => {
   const [location, setLocation] = useState<LatLngExpression | undefined>(undefined)
   const [marathonRoute, setMarathonRoute] = useState(null)
   const [lastUpdated, setLastUpdated] = useState<string | undefined>(undefined)
   const markerRef = useRef<any>(null)
-  const [minimalMarkerIcon, setMinimalMarkerIcon] = useState<Icon<IconOptions> | DivIcon | undefined >(undefined)
+  const [icon, setIcon] = useState<Icon<IconOptions> | DivIcon | undefined>(undefined)
 
   useEffect(() => {
-    const icon = new L.Icon({
+    const L = require("leaflet")
+
+    const newIcon = new L.Icon({
       iconUrl: "/face.png",
       iconSize: [25, 25],
       iconAnchor: [12, 25],
       popupAnchor: [0, -25],
     })
-    setMinimalMarkerIcon(icon)
+    setIcon(newIcon)
   }, [])
 
   useEffect(() => {
@@ -76,7 +85,7 @@ const LocationMap = () => {
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
-      {location && marathonRoute && (
+      {typeof window !== "undefined" && location && marathonRoute && (
         <MapContainer
           center={location}
           zoom={13}
@@ -88,7 +97,7 @@ const LocationMap = () => {
             url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
             attribution="Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
           />
-          <Marker ref={markerRef} position={location} icon={minimalMarkerIcon}>
+          <Marker ref={markerRef} position={location} icon={icon}>
             {lastUpdated && <Popup>Last updated: {lastUpdated}</Popup>}
           </Marker>
           <GeoJSON
